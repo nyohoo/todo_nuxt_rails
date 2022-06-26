f9ea1c258121e71e2682b93c1bb1e201f2f92273
nyohoo <nyohoo2013@gmail.com>
2022-06-26 09:35:27 +0900

    add:TODO登録コンポーネントの作成

---
+++ front/components/AddTodo.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col class="12" md="4">
+          <v-text-field
+            v-model="title"
+            counter="10"
+            label="todo"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col class="12" md="4">
+          <v-btn @click="handleSubmit">作成</v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.title = '';
+    },
+  },
+};
+</script>
+
+<style>
+</style>
