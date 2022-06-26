2125beee2a8e3fb8df8ce3daefc056235e1b6cec
nyohoo <nyohoo2013@gmail.com>
2022-06-27 03:26:05 +0900

    add:TODOの登録のリクエストを送り、保存されるのを確認

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -8,6 +8,7 @@
 <script>
 import TodoList from '@/components/TodoList';
 import AddTodo from '@/components/AddTodo';
+import axios from '@/plugins/axios';
 
 export default {
   components: {
@@ -20,7 +21,8 @@ export default {
     };
   },
   methods: {
-    addTodo(title) {
+    async addTodo(title) {
+      await axios.post('/v1/todos', { title });
       this.todos.push({
         title,
       });
