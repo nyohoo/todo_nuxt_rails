ce30cae16ff372a34256ff535c81364dd8755f57
nyohoo <nyohoo2013@gmail.com>
2022-06-26 12:02:13 +0900

    add: TODOリストコンポーネントの作成

---
+++ front/components/TodoList.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card>
+    <v-card-title>
+      Todo List
+      <v-spacer></v-spacer>
+      <v-text-field v-model="search" label="Search" single-line hide-details></v-text-field>
+    </v-card-title>
+    <v-data-table :headers="headers" :items="todos" :search="search"></v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      todos: [
+        {
+          title: "test",
+          username: "太郎"
+        }
+      ],
+      search: '',
+      headers: [
+        {
+          text: "タイトル",
+          align: "left",
+          sortable: false,
+          value: "title"
+        },
+        {
+          text: "ユーザー名",
+          value: "username"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+
+</style>
